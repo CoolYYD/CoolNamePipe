c7c10d7491a338ed986f9b35de6e3d3a65bb2ce9
CoolYYD <40992214+CoolYYD@users.noreply.github.com>
2020-06-12 16:32:41 +0800

    Add files via upload

---
+++ CoolNamePipe.Demo.Client1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoolNamePipe.Demo.Client1
+{
+    class Program
+    {
+        private static PipeClient pipeClient = new PipeClient("Client1");
+
+        static void Main(string[] args)
+        {
+            pipeClient.MessageReceived += PipeClient_MessageReceived;
+
+            while (true)
+            {
+                Thread.Sleep(100000000);
+            }
+        }
+
+        private static void PipeClient_MessageReceived(string message)
+        {
+            Console.WriteLine($"Client1 Get Message:{message}");
+            pipeClient.SendMessage("Client1:" + message);
+        }
+    }
+}
