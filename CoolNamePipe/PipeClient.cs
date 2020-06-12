eccea9d80e8d93373a7db38268b0bb0b4918ef31
CoolYYD <40992214+CoolYYD@users.noreply.github.com>
2020-06-12 16:29:52 +0800

    提交code

---
+++ CoolNamePipe/PipeClient.cs
@@ -0,0 +1,82 @@
+﻿using log4net;
+using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoolNamePipe
+{
+    public class PipeClient
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(PipeClient));
+
+        private bool isConnected;
+        NamedPipeClientStream pipeClient;
+        private int timeOut = 3000;
+        private string pipeName;
+
+        public delegate void MessageReceivedHandler(string message);
+        public event MessageReceivedHandler MessageReceived;
+
+        private const int PipeBufferSize = 65535;
+
+        public PipeClient(string pipeName)
+        {
+            this.pipeName = pipeName;
+            Task.Factory.StartNew(CreateClient);
+        }
+
+        private void CreateClient()
+        {
+            using (pipeClient = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut))
+            {
+                try
+                {
+                    Logger.Info("Client connecting PipeName:" + pipeName);
+                    pipeClient.Connect(timeOut);
+                    Logger.Info("Client is connected PipeName:" + pipeName);
+                    this.isConnected = true;
+                    while (true)
+                    {
+                        Thread.Sleep(1);
+                        byte[] data = new byte[PipeBufferSize];
+                        int read = pipeClient.Read(data, 0, data.Length);
+                        string resMsg = Encoding.UTF8.GetString(data, 0, read);
+                        pipeClient.FlushAsync();
+                        Logger.Info("Get Message:" + resMsg);
+                        if (string.IsNullOrEmpty(resMsg))
+                        {
+                            break;
+                        }
+                        this.MessageReceived?.Invoke(resMsg);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error("CreateClient err:" + ex.Message);
+                    this.isConnected = false;
+                }
+            }
+            Logger.Info("Client is disconnected:" + pipeName);
+            Thread.Sleep(1);
+            CreateClient();
+        }
+
+        public void SendMessage(string msg)
+        {
+            if (isConnected)
+            {
+                byte[] sendMsg = Encoding.UTF8.GetBytes(msg);
+                pipeClient.Write(sendMsg, 0, sendMsg.Length);
+            }
+            else
+            {
+                Logger.Error("SendMessage err, Server is disconnected:" + pipeName);
+            }
+        }
+    }
+}
+
+
---
+++ CoolNamePipe/PipeServer.cs
@@ -0,0 +1,101 @@
+﻿using log4net;
+using System;
+using System.Collections.Concurrent;
+using System.IO.Pipes;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoolNamePipe
+{
+    public class PipeServer
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(PipeServer));
+        private NamedPipeServerStream pipeServer;
+        private string pipeName;
+        public ConcurrentQueue<Message> sendMsgQueue = new ConcurrentQueue<Message>();
+        private const int PipeBufferSize = 65535;
+
+        private bool isConnected;
+        private Message sendMsg;
+
+        public PipeServer(string pipeName)
+        {
+            this.pipeName = pipeName;
+            Task.Factory.StartNew(CreateServer);
+        }
+
+        private void CreateServer()
+        {
+            try
+            {
+                using (pipeServer = new NamedPipeServerStream(pipeName, PipeDirection.InOut))
+                {
+                    Logger.Info("Wait Client connect PipeName:" + pipeName);
+                    pipeServer.WaitForConnection();
+                    Logger.Info("Client is connected PipeName:" + pipeName);
+                    this.isConnected = true;
+
+                    while (true)
+                    {
+                        Thread.Sleep(1);
+                        bool isSuccess = sendMsgQueue.TryDequeue(out sendMsg);
+                        if (isSuccess)
+                        {
+                            byte[] arrByte = Encoding.UTF8.GetBytes(sendMsg.sendContent);
+                            pipeServer.Write(arrByte, 0, arrByte.Length);
+                            byte[] data = new byte[PipeBufferSize];
+                            int read = pipeServer.Read(data, 0, data.Length);
+                            sendMsg.resContent = Encoding.UTF8.GetString(data, 0, read);
+                            pipeServer.FlushAsync();
+                            sendMsg.areReceive.Set();
+                            if (string.IsNullOrEmpty(sendMsg.resContent) && !pipeServer.IsConnected)
+                            {
+                                this.isConnected = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("CreateServer err:" + ex);
+                this.isConnected = false;
+                sendMsg.areReceive.Set();
+            }
+            Thread.Sleep(1);
+            CreateServer();
+        }
+
+
+        public string SendMessage(string msg)
+        {
+            if (this.isConnected)
+            {
+                Message sendMsg = new Message(msg);
+                sendMsgQueue.Enqueue(sendMsg);
+                sendMsg.areReceive.WaitOne();
+                return sendMsg.resContent;
+            }
+            else
+            {
+                Logger.Warn("PipeServer is disconnected:PipeName:" + pipeName);
+                return null;
+            }
+        }
+    }
+
+    public class Message
+    {
+        public Message(string msg)
+        {
+            areReceive = new AutoResetEvent(false);
+            sendContent = msg;
+        }
+
+        public AutoResetEvent areReceive { get; set; }
+        public string sendContent { get; set; }
+        public string resContent { get; set; }
+    }
+}
